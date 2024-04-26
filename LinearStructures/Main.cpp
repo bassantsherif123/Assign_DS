dc5a25059e231614e358bbf1a077b1c548939e38
bassantsherif123 <117759228+bassantsherif123@users.noreply.github.com>
2024-04-26 11:25:00 +0200

    Create Main.cpp

---
+++ LinearStructures/Main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include "../LinearStructures/SingleLinkedList.cpp"
+#include "../LinearStructures/DoubleLinkedList.cpp"
+#include "../LinearStructures/CircularLinkedList.cpp"
+//#include "../LinearStructures/Stack.cpp"
+#include "../LinearStructures/Queue.cpp"
+using namespace std;
+
+int main() {
+    SingleLinkedList<int> s;
+    DoubleLinkedList<int> d;
+    CircularLinkedList<int> myList;
+    Queue<int>q;
+
+    return 0;
+}
