87922e2d807dcb8ddeed07cd75263a0a1721e793
Alshimaaaa <125492024+Alshimaaaa@users.noreply.github.com>
2024-05-18 04:41:14 -0700

    comments Heap.cpp

--- Non-Linear-Structures/Heap.cpp
+++ Non-Linear-Structures/Heap.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 
-void heapify(vector<Item>&a, int n, int i, int choice){
+void heapify(vector<Item>&a, int n, int i, int choice){//Max heapify or Min heapify
     if(choice==1) {
         int largest =i;
         int left=(2*i + 1), right=(2*i +2);
@@ -46,13 +46,13 @@ void heapify(vector<Item>&a, int n, int i, int choice){
     }
 }
 
-void heapSort(vector<Item>&a, int n, int choice){
+void heapSort(vector<Item>&a, int n, int choice){//The main heap sort function
     for(int i=(n/2-1);i>=0;i--){
-        heapify(a,n,i,choice);
+        heapify(a,n,i,choice);//build the heap
     }
     for(int i = n -1;i>=0;i--){
-        swap(a[0], a[i]);
-        heapify(a,i,0,choice);
+        swap(a[0], a[i]);//arrange the array in an order(the first element is arranged)
+        heapify(a,i,0,choice);//build the heap again for the rest of the items
     }
 }
 class heap {
@@ -62,7 +62,7 @@ public:
     void add(Item it) {
         items.push_back(it);
     }
-    void remove(Item it) {
+    void remove(Item it) {//search for the item by its name then remove it
         bool flag = true;
         for(int i=0;i<items.size();i++) {
             if(items[i] == it) {
@@ -79,12 +79,12 @@ public:
         }
     }
 
-    void sortPrice(int choice) {
+    void sortPrice(int choice) {//take the choice of the user of how he wants to arrange and then send it to the sorting function 
         vector<Item> copy;
         copy = items;
         heapSort(copy,copy.size(),choice);
 
-        for (auto& item : copy) {
+        for (auto& item : copy) {//display the sorted items
             item.print();
         }
     }
